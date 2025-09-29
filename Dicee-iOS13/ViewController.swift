97d2464b42e4496afe3ca81affd1b43625d69a76
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2025-09-28 21:24:41 -0300

    feat: adding random dice face when button is pressed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+//	single line comment
+	/*
+	 this is a multi-line comment
+	 */
 	
 //	IBOutlet allows me to reference a UI Element
 	@IBOutlet weak var diceImageView1: UIImageView!
 	@IBOutlet weak var diceImageView2: UIImageView!
+	
+	
+	let diceFaces = [
+		UIImage(named: "DiceOne"),
+		UIImage(named: "DiceTwo"),
+		UIImage(named: "DiceThree"),
+		UIImage(named: "DiceFour"),
+		UIImage(named: "DiceFive"),
+		UIImage(named: "DiceSix")
+	]
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -24,7 +38,8 @@ class ViewController: UIViewController {
 	}
 //	Interface Build Action = IBAction
 	@IBAction func rollButtonPressed(_ sender: UIButton) {
-		diceImageView1.image = UIImage(named: "DiceFour")
-		diceImageView2.image = UIImage(named: "DiceFour")
+		
+		diceImageView1.image = diceFaces.randomElement()!!
+		diceImageView2.image = diceFaces.randomElement()!!
 	}
 }
